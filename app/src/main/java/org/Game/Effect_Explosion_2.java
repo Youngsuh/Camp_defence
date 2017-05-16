53533e059faf4635e26c3deab153b67a85347d1c
Kim Youngsuh <abcdmat@naver.com>
2017-05-16 11:14:07 +0900

    몹 사망시 이펙트 생성  - 지우는건 아직
    다음 예정 - 아이템 생성, 사운드 풀
    
    못한것 - 백그라운드 사이즈 조정, 메인페이지

--- app/src/main/java/org/Game/Effect_Explosion_2.java
+++ app/src/main/java/org/Game/Effect_Explosion_2.java
@@ -5,9 +5,9 @@ import org.Framework.GraphicObject;
 import org.Framework.R;
 import org.Framework.SpriteAnimation;
 
-public class Effect_Explosion_2 extends GraphicObject {
+public class Effect_Explosion_2 extends Effect_Explosion {
 
-	public Effect_Explosion_2(int num, int x , int y) {
+	public Effect_Explosion_2( int x , int y) {
 		super(AppManager.getInstance().getBitmap(R.drawable.yeti_die));
 		this.SetPosition(139,120);
 		m_x = x;
